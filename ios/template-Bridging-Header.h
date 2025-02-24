02d3b372670159d149b8c39a536de09354db8935
Trương Huỳnh Đức <ducth@Truongs-MacBook-Pro.local>
2025-02-24 17:47:12 +0700

    update version react-native 0.73.4 -> 0.77.1

---
+++ ios/template-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  template-Bridging-Header.h
+//  template
+//
+//  Created by Trương Huỳnh Đức on 24/02/2025.
+//
+
+#ifndef template_Bridging_Header_h
+#define template_Bridging_Header_h
+
+
+#endif /* template_Bridging_Header_h */
